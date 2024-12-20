19c7a330d17606dece6ca726c7f63b9ed91abbc2
Mohamed Yusuf <mohamed208.y@gmail.com>
2024-12-20 18:25:54 +0300

    corr based graphs

--- excel_to_nivo/main.py
+++ excel_to_nivo/main.py
@@ -18,11 +18,11 @@ def match_length(arr1, arr2):
     
     return adjusted_arr1, adjusted_arr2
 
-def percentage_helper(corr):
-    if(corr < 0):
+def percentage_helper(list1, listy2):
+    if(round(list1.corr(listy2), 2) < 0):
         return 0
     else:
-        corr * 100
+        return round(list1.corr(listy2) * 100, 1) 
 
 @app.route("/bar1")
 def process_clean_data_for_bar1():
@@ -42,53 +42,100 @@ def process_clean_data_for_bar1():
 
     owner_np = Owner.to_numpy()[~np.isnan(Owner.to_numpy())]
     q9_phone_np = q9_phone.to_numpy()[~np.isnan(q9_phone.to_numpy())]
-
     owner_np, q9_phone_np = match_length(owner_np, q9_phone_np)
-    
-    #table1 = pd.crosstab(index=owner_np, columns=q9_phone_np) # this is a contingency table
-    #overall_proportions = table1 / table1.sum().sum()
-
-    print(Owner.corr(q9_phone))
 
     roles_how_informed_data = [{
                     "role": "Owner", 
-                    "phone": round(Owner.corr(q9_phone), 2), 
-                    "news": round(Owner.corr(q9_news), 2),
-                    "tv": round(Owner.corr(q9_tv),2),
-                    "family": round(Owner.corr(q9_family),2),
-                    "parent": round(Owner.corr(q9_parent), 2),
-                    "unkown": round(Owner.corr(q9_unknown),2),
+                    "phone": percentage_helper(Owner, q9_phone), 
+                    "news": percentage_helper(Owner, q9_news),
+                    "tv": percentage_helper(Owner, q9_tv),
+                    "family": percentage_helper(Owner, q9_family),
+                    "parent": percentage_helper(Owner, q9_parent),
+                    "unknown": percentage_helper(Owner, q9_unknown),
                     },
                     {
                     "role": "Manager", 
-                    "phone": round(Manager.corr(q9_phone), 2), 
-                    "news": round(Manager.corr(q9_news), 2),
-                    "tv": round(Manager.corr(q9_tv),2),
-                    "family": round(Manager.corr(q9_family),2),
-                    "parent": round(Manager.corr(q9_parent), 2),
-                    "unkown": round(Manager.corr(q9_unknown),2),
+                    "phone": percentage_helper(Manager, q9_phone), 
+                    "news": percentage_helper(Manager, q9_news),
+                    "tv": percentage_helper(Manager, q9_tv),
+                    "family": percentage_helper(Manager, q9_family),
+                    "parent": percentage_helper(Manager, q9_parent),
+                    "unknown": percentage_helper(Manager, q9_unknown),
                     },
                     {
                     "role": "Teacher", 
-                    "phone": round(Teacher.corr(q9_phone), 2), 
-                    "news": round(Teacher.corr(q9_news), 2),
-                    "tv": round(Teacher.corr(q9_tv),2),
-                    "family": round(Teacher.corr(q9_family),2),
-                    "parent": round(Teacher.corr(q9_parent), 2),
-                    "unkown": round(Teacher.corr(q9_unknown),2),
-                    },
+                    "phone": percentage_helper(Teacher, q9_phone), 
+                    "news": percentage_helper(Teacher, q9_news),
+                    "tv": percentage_helper(Teacher, q9_tv),
+                    "family": percentage_helper(Teacher, q9_family),
+                    "parent": percentage_helper(Teacher, q9_parent),
+                    "unknown": percentage_helper(Teacher, q9_unknown),
+                    }, 
                     {
-                    "role": "Other", 
-                    "phone": round(Other.corr(q9_phone), 2), 
-                    "news": round(Other.corr(q9_news), 2),
-                    "tv": round(Other.corr(q9_tv),2),
-                    "family": round(Other.corr(q9_family),2),
-                    "parent": round(Other.corr(q9_parent), 2),
-                    "unkown": round(Other.corr(q9_unknown),2),
+                        "role": "Other", 
+                        "phone": percentage_helper(Other, q9_phone), 
+                        "news": percentage_helper(Other, q9_news),
+                        "tv": percentage_helper(Other, q9_tv),
+                        "family": percentage_helper(Other, q9_family),
+                        "parent": percentage_helper(Other, q9_parent),
+                        "unknown": percentage_helper(Other, q9_unknown),
                     }
                    ]
 
     return jsonify(roles_how_informed_data)
+
+@app.route("/bar2")
+def process_clean_data_for_bar2():
+    masterData = pd.read_excel('masterData.xlsx', index_col=0)
+
+    Owner = masterData['Q3)  role_Owner']
+    Manager = masterData['Q3)  role_Manager']
+    Teacher = masterData['Q3) role_Teacher']
+    Other = masterData['Q3) other_Role']
+
+    Q10_Computer_Alert_words = masterData['Q10_Computer_Alert_words']
+    Q10b_fire_over_cell_Words = masterData['Q10b_fire_over_cell_Words\n\n']
+    Q10_Unknown_words = masterData['Q10_Unknown_words']
+    Q10_Television_Radio_words = masterData['Q10_Television_Radio_words']
+    Q10_Parent_Guardian_words = masterData['Q10_Parent_Guardian_words']
+    Q10_Other_words = masterData['Q10_Other_words']
+    Q10_Family_Friend_words = masterData['Q10_Family_Friend_words']
+    Q10_Supervisor_words = masterData['Q10_Supervisor_words']
+
+    roles_how_informed_data = [
+            { "role": "Owner", 
+             "cell_words": percentage_helper(Owner, Q10b_fire_over_cell_Words), 
+             "tv": percentage_helper(Owner, Q10_Television_Radio_words), 
+             "family": percentage_helper(Owner, Q10_Family_Friend_words),
+             "parent": percentage_helper(Owner, Q10_Parent_Guardian_words),
+             "unknown": percentage_helper(Owner, Q10_Unknown_words)
+             },
+            { "role": "Manager", 
+             "cell_words": percentage_helper(Manager, Q10b_fire_over_cell_Words), 
+             "tv": percentage_helper(Manager, Q10_Television_Radio_words), 
+             "family": percentage_helper(Manager, Q10_Family_Friend_words),
+             "parent": percentage_helper(Manager, Q10_Parent_Guardian_words),
+             "unknown": percentage_helper(Manager, Q10_Unknown_words)
+             },
+            { "role": "Teacher", 
+             "cell_words": percentage_helper(Teacher, Q10b_fire_over_cell_Words), 
+             "tv": percentage_helper(Teacher, Q10_Television_Radio_words), 
+             "family": percentage_helper(Teacher, Q10_Family_Friend_words),
+             "parent": percentage_helper(Teacher, Q10_Parent_Guardian_words),
+             "unknown": percentage_helper(Teacher, Q10_Unknown_words)
+             },
+            { "role": "Other", 
+             "cell_words": percentage_helper(Other, Q10b_fire_over_cell_Words), 
+             "tv": percentage_helper(Other, Q10_Television_Radio_words), 
+             "family": percentage_helper(Other, Q10_Family_Friend_words),
+             "parent": percentage_helper(Other, Q10_Parent_Guardian_words),
+             "unknown": percentage_helper(Other, Q10_Unknown_words)
+             }
+
+    ]
+   
+    return jsonify(roles_how_informed_data)
+ 
 
 
 if __name__ == '__main__':
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -105,6 +105,7 @@ const topProducts = [
 
 const Dashboard = () => {
   const [barData, setBarData] = useState([]);
+  const [barData2, setBarData2] = useState([]);
 
   useEffect(() => {
     fetch('http://127.0.0.1:5000/bar1', {
@@ -116,8 +117,22 @@ const Dashboard = () => {
     }).then(res => res.json()).
       then(res => {
         if (res.constructor === Array) {
-          console.log(res)
           setBarData(res)
+        }
+      })
+  }, [])
+ 
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/bar2', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(res => res.json()).
+      then(res => {
+        if (res.constructor === Array) {
+          setBarData2(res)
         }
       })
   }, [])
@@ -169,6 +184,48 @@ const Dashboard = () => {
               <ResponsiveBar
                 data={barData}
                 keys={['family', 'news', 'parent', 'phone', 'tv', 'unknown']}
+                indexBy="role"
+                margin={{ top: 20, right: 130, bottom: 50, left: 60 }}
+                padding={0.3}
+                valueScale={{ type: 'linear' }}
+                indexScale={{ type: 'band', round: true }}
+                colors={{ scheme: 'nivo' }}
+                axisTop={null}
+                axisRight={null}
+                axisBottom={{
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                }}
+                axisLeft={{
+                  tickSize: 5,
+                  tickPadding: 5,
+                  tickRotation: 0,
+                }}
+                labelSkipWidth={12}
+                labelSkipHeight={12}
+                legends={[
+                  {
+                    dataFrom: 'keys',
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 120,
+                    translateY: 0,
+                    itemsSpacing: 2,
+                    itemWidth: 100,
+                    itemHeight: 20,
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 0.85,
+                    symbolSize: 20,
+                  },
+                ]}
+              />
+            </Box>
+            <Box sx={{ height: 350 }}>
+              <ResponsiveBar
+                data={barData2}
+                keys={['family', 'cell_words', 'parent', 'alert', 'tv', 'unknown']}
                 indexBy="role"
                 margin={{ top: 20, right: 130, bottom: 50, left: 60 }}
                 padding={0.3}
